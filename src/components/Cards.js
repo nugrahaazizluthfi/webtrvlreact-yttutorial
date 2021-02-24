456d2135bba01e22e7e3efe88949e5b5d0492441
Luthfi Aziz Nugraha <nugrahaazizluthfi@yahoo.com>
2021-02-24 10:32:32 +0700

    add footer section & finish tutorial

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
+import Image9 from '../assets/images/img-9.jpg';
+import Image2 from '../assets/images/img-2.jpg';
+import Image3 from '../assets/images/img-3.jpg';
+import Image4 from '../assets/images/img-4.jpg';
+import Image8 from '../assets/images/img-8.jpg';
 
 function Cards() {
   return (
@@ -10,13 +15,13 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="/images/img-9.jpg"
+              src={Image9}
               text="Explore the hidden watterfall deep inside the Amazon Jungle"
               label="Adventure"
               path="/services"
             />
             <CardItem
-              src="/images/img-2.jpg"
+              src={Image2}
               text="Travel through is Islands of Bali in Private Cruise"
               label="Luxury"
               path="/services"
@@ -24,19 +29,19 @@ function Cards() {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="images/img-3.jpg"
+              src={Image3}
               text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
               label="Mystery"
               path="/services"
             />
             <CardItem
-              src="images/img-4.jpg"
+              src={Image4}
               text="Experience Football on Top of the Himilayan Mountains"
               label="Adventure"
               path="/products"
             />
             <CardItem
-              src="images/img-8.jpg"
+              src={Image8}
               text="Ride through the Sahara Desert on a guided camel tour"
               label="Adrenaline"
               path="/sign-up"
