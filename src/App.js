e8d50574eae8df278c39a4efd62deb449aa14de6
Luthfi Aziz Nugraha <nugrahaazizluthfi@yahoo.com>
2021-02-24 09:09:40 +0700

    add service page, product page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Navbar></Navbar>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/services" exact component={Services} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/sign-up" exact component={SignUp} />
         </Switch>
       </Router>
     </>
